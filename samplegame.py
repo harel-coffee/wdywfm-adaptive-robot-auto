199232ac85a419589e151484a4521886a1e9a97f
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2021-09-24 16:47:30 +0100

    Changing payoffs, in terms of expected people rescued

--- samplegame.py
+++ samplegame.py
@@ -1,5 +1,7 @@
 import logging
 
+import gambit
+
 from game import InteractionGame
 
 FOLLOW_ME_ROBOT_ACTION = "follow_me"
@@ -7,6 +9,15 @@ WAIT_HERE_ROBOT_ACTION = "wait_here"
 
 ZERO_RESPONDER_TYPE = "ZERO_RESPONDER"
 SELFISH_TYPE = "SELFISH"
+
+# Identity impact = 0.3
+# Unassisted success = 0.8
+# Victim success = 0.2
+
+FOLLOWME_SUCCESS_ROBOT_PAYOFF = 3  # Victim and Player with R + Extra with FR
+FOLLOWME_FAIL_ROBOT_PAYOFF = gambit.Rational(11, 5)  # Extra with FR + Player with R + Victim unassisted
+WAITHERE_SUCCESS_ROBOT_PAYOFF = gambit.Rational(14, 5)  # Victim and Player with FR + Extra unassisted
+WAITHERE_FAIL_ROBOT_PAYOFF = gambit.Rational(13, 5)  # Victim with FR + Player unassisted + Extra unassisted
 
 
 def generate_game_model(zero_responder_ratio, selfish_ratio, filename="game_file.efg"):
@@ -18,15 +29,48 @@ def generate_game_model(zero_responder_ratio, selfish_ratio, filename="game_file
     interaction_game.set_first_contact_actions([FOLLOW_ME_ROBOT_ACTION, WAIT_HERE_ROBOT_ACTION])
 
     interaction_game.configure_person_response(ZERO_RESPONDER_TYPE, FOLLOW_ME_ROBOT_ACTION,
-                                               [("coming_together", 2, 2), ("coming_alone", -2, -2)])
+                                               [(
+                                                   "coming_together",
+                                                   FOLLOWME_SUCCESS_ROBOT_PAYOFF,
+                                                   gambit.Rational(13, 10)),  # 1 + Identity impact
+                                                   ("coming_alone",
+                                                    FOLLOWME_FAIL_ROBOT_PAYOFF,
+                                                    gambit.Rational(7, 10))])  # 1 - Identity impact
     interaction_game.configure_person_response(ZERO_RESPONDER_TYPE, WAIT_HERE_ROBOT_ACTION,
-                                               [("wait_together", 1, 2), ("leave_alone", -1, -2)])
+                                               [("wait_together",
+                                                 WAITHERE_SUCCESS_ROBOT_PAYOFF,
+                                                 gambit.Rational(13, 10)),  # 1 + Identity impact
+                                                ("leave_alone",
+                                                 WAITHERE_FAIL_ROBOT_PAYOFF,
+                                                 gambit.Rational(1, 2))])  # Unassisted success - Identity impact
     interaction_game.configure_person_response(SELFISH_TYPE, FOLLOW_ME_ROBOT_ACTION,
-                                               [("coming_together", 2, -1), ("coming_alone", -2, 2)])
+                                               [(
+                                                   "coming_together",
+                                                   FOLLOWME_SUCCESS_ROBOT_PAYOFF,
+                                                   gambit.Rational(7, 10)),  # 1 - Identity impact
+                                                   ("coming_alone",
+                                                    FOLLOWME_FAIL_ROBOT_PAYOFF,
+                                                    gambit.Rational(13, 10))])  # 1 + Identity impact
     interaction_game.configure_person_response(SELFISH_TYPE, WAIT_HERE_ROBOT_ACTION,
-                                               [("wait_together", 1, -1), ("leave_alone", -1, 0)])
+                                               [("wait_together",
+                                                 WAITHERE_SUCCESS_ROBOT_PAYOFF,
+                                                 gambit.Rational(7, 10)),  # 1 - Identity impact
+                                                ("leave_alone",
+                                                 WAITHERE_FAIL_ROBOT_PAYOFF,
+                                                 gambit.Rational(11, 10))])  # Unassisted success + Identity impact
 
     if filename:
         interaction_game.write(filename)
 
     return interaction_game
+
+
+def main():
+    zero_responder_ratio = 0.5.as_integer_ratio()
+    selfish_ratio = 0.5.as_integer_ratio()
+    generate_game_model(zero_responder_ratio, selfish_ratio, filename="game_file.efg")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    main()
