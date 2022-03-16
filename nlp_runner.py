096e0293e49533fea10a3bb6695017687c903c47
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2022-03-16 16:53:51 +0000

    Upsampling minority class for NB classifier

--- nlp_runner.py
+++ nlp_runner.py
@@ -3,16 +3,18 @@ import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
 
 from analyser import NaiveBayesTypeAnalyser, TunedTransformerTypeAnalyser
 from controller import AutonomicManagerController
 from environment import PERSONAL_IDENTITY_CLASS, GROUP_IDENTITY_CLASS, CLASS_TO_TYPE, EmergencyEvacuationEnvironment
 from synthetic_runner import INTERACTIONS_PER_SCENARIO, NUM_SCENARIOS, run_scenario, SEED
+from imblearn.over_sampling import RandomOverSampler
 
 TEXT_CONTENT_COLUMN = "text"
-TEXT_LABEL_COLUMN = "will_help"
+TEXT_LABEL_COLUMN = "label"
+GROUP_IDENTITY_LABEL = "Group"
 
 
 def plot_word_counts(tweets_dataframe):
@@ -25,8 +27,13 @@ def plot_word_counts(tweets_dataframe):
 
 
 def get_dataset():
-    tweets_dataframe = pd.read_csv("data/survivor_responses.csv")
-    tweets_dataframe[TEXT_LABEL_COLUMN] = tweets_dataframe[TEXT_LABEL_COLUMN].astype(int)
+    tweets_dataframe = pd.read_csv("data/emergency_interactions.csv")
+    logging.info("Before label consolidation")
+    logging.info(tweets_dataframe[TEXT_LABEL_COLUMN].value_counts())
+
+    tweets_dataframe[TEXT_LABEL_COLUMN] = tweets_dataframe[TEXT_LABEL_COLUMN].map(
+        lambda label: GROUP_IDENTITY_CLASS if label == GROUP_IDENTITY_LABEL else PERSONAL_IDENTITY_CLASS)
+    logging.info("After label consolidation")
     logging.info(tweets_dataframe[TEXT_LABEL_COLUMN].value_counts())
     return tweets_dataframe
 
@@ -44,15 +51,30 @@ def configure_tuned_transformer(tweets_dataframe, test_size, column_for_stratify
     return type_analyser, text_test_features, label_test_array
 
 
+def upsample_minority_class(text_train, label_train):
+    text_train = np.expand_dims(text_train, axis=1)
+    over_sampler = RandomOverSampler(random_state=SEED)
+
+    text_train, label_train = over_sampler.fit_resample(text_train, label_train)
+    text_train = np.squeeze(text_train)
+
+    logging.info("text_train.shape {}".format(text_train.shape))
+    logging.info("label_train.shape {}".format(label_train.shape))
+
+    return text_train, label_train
+
+
 def configure_naive_bayes(tweets_dataframe, test_size):
     tweet_label = tweets_dataframe.pop(TEXT_LABEL_COLUMN)
     tweet_text = tweets_dataframe.pop(TEXT_CONTENT_COLUMN)
+
     text_train, text_test, label_train, label_test = train_test_split(tweet_text,
                                                                       tweet_label,
                                                                       stratify=tweet_label,
                                                                       test_size=test_size,
                                                                       random_state=SEED)
 
+    text_train, label_train = upsample_minority_class(text_train, label_train)
     type_analyser = NaiveBayesTypeAnalyser()
     type_analyser.train(text_train, label_train)
 
@@ -64,7 +86,7 @@ def configure_naive_bayes(tweets_dataframe, test_size):
     logging.info(
         classification_report(label_test, label_test_predicted, target_names=[CLASS_TO_TYPE[PERSONAL_IDENTITY_CLASS],
                                                                               CLASS_TO_TYPE[GROUP_IDENTITY_CLASS]]))
-
+    logging.info("accuracy_score: {}".format(accuracy_score(label_test, label_test_predicted)))
     return type_analyser, text_test_features, label_test_array
 
 
@@ -72,10 +94,10 @@ def main():
     test_size = 0.5
     tweets_dataframe = get_dataset()
 
-    # type_analyser, text_test_features, label_test_array = configure_naive_bayes(tweets_dataframe, test_size)
-    type_analyser, text_test_features, label_test_array = configure_tuned_transformer(tweets_dataframe, test_size,
-                                                                                      TEXT_LABEL_COLUMN, SEED,
-                                                                                      train=True)
+    type_analyser, text_test_features, label_test_array = configure_naive_bayes(tweets_dataframe, test_size)
+    # type_analyser, text_test_features, label_test_array = configure_tuned_transformer(tweets_dataframe, test_size,
+    #                                                                                   TEXT_LABEL_COLUMN, SEED,
+    #                                                                                   train=True)
 
     robot_controller = AutonomicManagerController(type_analyser)
     emergency_environment = EmergencyEvacuationEnvironment(text_test_features, label_test_array,
