d453dc64c7c60aae245d907e714cf11898aeeb1e
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2022-08-04 19:46:18 +0100

    Analysing simulation output.

--- abm_analysis.py
+++ abm_analysis.py
@@ -1,35 +1,102 @@
+import numpy as np
 import pyNetLogo
 import pandas as pd
+from typing import List
 
-TURTLE_PRESENT_REPORTER = "count turtles"
-EVACUATED_REPORTER = "number_passengers - count agents + 1"
-DEAD_REPORTER = "count agents with [ st_dead = 1 ]"
+import matplotlib.pyplot as plt
+from scipy.stats import mannwhitneyu
+
+TURTLE_PRESENT_REPORTER = "count turtles"  # type:str
+EVACUATED_REPORTER = "number_passengers - count agents + 1"  # type:str
+DEAD_REPORTER = "count agents with [ st_dead = 1 ]"  # type:str
+
+ENABLE_STAFF_COMMAND = "set REQUEST_STAFF_SUPPORT TRUE"  # type:str
+
+RESULTS_CSV_FILE = "data/experiment_results.csv"  # type:str
+NO_SUPPORT_COLUMN = "no-support"  # type:str
+ONLY_STAFF_SUPPORT_COLUMN = "staff-support"  # type:str
+
+SAMPLES = 250  # type:int
 
 
-def run_simulation(netlogo_link):
-    # type: (pyNetLogo.NetLogoLink) -> float
+# Using https://www.stat.ubc.ca/~rollin/stats/ssize/n2.html
+# And https://www.statology.org/pooled-standard-deviation-calculator/
+
+
+def run_simulation(netlogo_link, post_setup_command=""):
+    # type: (pyNetLogo.NetLogoLink, str) -> float
     netlogo_link.command("setup")
+    if post_setup_command:
+        netlogo_link.command(post_setup_command)
+        print("{0} executed".format(post_setup_command))
+
     metrics_dataframe = netlogo_link.repeat_report(
         netlogo_reporter=[TURTLE_PRESENT_REPORTER, EVACUATED_REPORTER, DEAD_REPORTER], reps=2000)  # type: pd.DataFrame
-
-    print(metrics_dataframe)
 
     evacuation_finished = metrics_dataframe[
         metrics_dataframe[TURTLE_PRESENT_REPORTER] == metrics_dataframe[DEAD_REPORTER]]
 
     evacuation_time = evacuation_finished.index.min()  # type: float
+    print("evacuation time {0}".format(evacuation_time))
 
     return evacuation_time
 
 
-def main(netlogo_home, netlogo_version, model_file):
+def start_experiments(netlogo_home, netlogo_version, model_file, gui=True):
     netlogo_link = pyNetLogo.NetLogoLink(netlogo_home=netlogo_home,
                                          netlogo_version=netlogo_version,
-                                         gui=True)
+                                         gui=gui)  # type: pyNetLogo.NetLogoLink
     netlogo_link.load_model(model_file)
 
-    evacuation_time = run_simulation(netlogo_link)  # type:float
-    print("evacuation_time {0}".format(evacuation_time))
+    no_support_times = [run_simulation(netlogo_link)
+                        for _ in range(SAMPLES)]  # type:List[float]
+    staff_support_times = [run_simulation(netlogo_link, post_setup_command=ENABLE_STAFF_COMMAND)
+                           for _ in range(SAMPLES)]  # type:List[float]
+
+    experiment_results = pd.DataFrame(data=list(zip(no_support_times, staff_support_times)),
+                                      columns=[NO_SUPPORT_COLUMN, ONLY_STAFF_SUPPORT_COLUMN])  # type:pd.DataFrame
+
+    experiment_results.to_csv(RESULTS_CSV_FILE)
+    print("Data written to {}".format(RESULTS_CSV_FILE))
+
+
+def analyse_results():
+    figure, axes = plt.subplots()
+
+    results_dataframe = pd.read_csv(RESULTS_CSV_FILE)  # type: pd.DataFrame
+    results_dataframe = results_dataframe.dropna()
+
+    evacuation_no_support = results_dataframe[NO_SUPPORT_COLUMN].values  # type: List[float]
+    evacuation_staff_support = results_dataframe[ONLY_STAFF_SUPPORT_COLUMN].values  # type: List[float]
+    print(
+        "np.mean(evacuation_no_support) = {} np.std(evacuation_no_support) = {}".format(np.mean(evacuation_no_support),
+                                                                                        np.std(evacuation_no_support)))
+    print("np.mean(evacuation_staff_support) = {} np.std(evacuation_staff_support) = {}".format(
+        np.mean(evacuation_staff_support),
+        np.std(evacuation_staff_support)))
+
+    null_hypothesis = "The distribution of {} times is THE SAME as the distribution of {} times".format(
+        NO_SUPPORT_COLUMN, ONLY_STAFF_SUPPORT_COLUMN)  # type: str
+
+    threshold = 0.05  # type:float
+    u, p_value = mannwhitneyu(x=evacuation_no_support, y=evacuation_staff_support)
+    print("U={} , p={}".format(u, p_value))
+    if p_value > threshold:
+        print("FAILS TO REJECT: {}".format(null_hypothesis))
+    else:
+        print("REJECT: {}".format(null_hypothesis))
+
+    _ = axes.violinplot([evacuation_no_support,
+                         evacuation_staff_support], showmeans=True)
+    axes.set_ylabel("Evacuation Time")
+
+    labels = ["no-support", "staff-support"]  # List[str]
+    axes.xaxis.set_tick_params(direction='out')
+    axes.xaxis.set_ticks_position('bottom')
+    axes.set_xticks(np.arange(1, len(labels) + 1))
+    axes.set_xlim(0.25, len(labels) + 0.75)
+
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -37,4 +104,7 @@ if __name__ == "__main__":
     netlogo_directory = "/home/cgc87/netlogo-5.3.1-64"
     version = "5"
 
-    main(netlogo_directory, version, netlogo_model)
+    # start_experiments(netlogo_directory, version, netlogo_model, gui=False)
+
+    plt.style.use('seaborn-darkgrid')
+    analyse_results()
